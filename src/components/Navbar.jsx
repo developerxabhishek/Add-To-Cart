3b779577264167363b922120e2c6355ac69bf95b
Abhishek Pandey <abhishekpandey095155@gmail.com>
2024-04-22 00:37:43 +0530

    Updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,29 +1,48 @@
-import React from 'react'
-import { useState } from 'react';
+import React from "react";
+import { useState } from "react";
 import { FaShoppingCart } from "react-icons/fa";
 import { CiForkAndKnife } from "react-icons/ci";
-import { useSelector, useDispatch } from 'react-redux'
+import { useSelector, useDispatch } from "react-redux";
 import CartPage from "../pages/CartPage";
 const Navbar = () => {
-  const [state,setState]=useState("hide")
-  const mySelector=useSelector((state)=>state.cartSlice.cart);
-
-const dispatch=useDispatch();
+  const [state, setState] = useState("hide");
+  const mySelector = useSelector((state) => state.cartSlice.cart);
+  console.log(mySelector);
+  const displayCart = (state) => {
+    if (state) {
+      setState("show");
+    } else {
+      setState("hide");
+    }
+  };
+  const dispatch = useDispatch();
   return (
-<>
-<div className="nav">
-<div className="nav-icon-container">
-<div><CiForkAndKnife style={{fontSize:"25px "}}/></div>
-<h3>Food's Restaurant</h3></div>
-<div className='nav-cart'>
-<FaShoppingCart style={{fontSize:"25px",color:"black"}} onClick={()=>{setState("")}}/>
-<span className='cart-quantity-display'>{mySelector.length}</span>
-</div>
-</div>
-<div className={state}>
-<CartPage/>
-</div>
-</>
-  )
-}
-export default Navbar
+    <>
+      <div className="nav">
+        <div className="nav-icon-container">
+          <div>
+            <CiForkAndKnife style={{ fontSize: "25px " }} />
+          </div>
+          <h3>Food's Restaurant</h3>
+        </div>
+        <div className="nav-cart">
+          <FaShoppingCart
+            style={{ fontSize: "25px", color: "white" }}
+            onClick={() => {
+              if (state === "show") {
+                displayCart(false);
+              } else {
+                displayCart(true);
+              }
+            }}
+          />
+          <span className="cart-quantity-display">{mySelector.length}</span>
+        </div>
+      </div>
+      <div className={state}>
+        <CartPage displayCart={displayCart} />
+      </div>
+    </>
+  );
+};
+export default Navbar;
--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -1,63 +1,96 @@
-import { useSelector, useDispatch } from 'react-redux'
-import { addToCart,decqty } from "../Slices/cartSlice"
-import { useState } from 'react';
-import { Link } from 'react-router-dom';
-const CartPage = () => {
-  const [state,setState]=useState("")
-   const mySelector=useSelector((state)=>state.cartSlice.cart);
-  const dispatch=useDispatch();
+import { useSelector, useDispatch } from "react-redux";
+import { addToCart, decqty } from "../Slices/cartSlice";
+import { Link } from "react-router-dom";
+const CartPage = ({ displayCart }) => {
+  const mySelector = useSelector((state) => state.cartSlice.cart);
+  const dispatch = useDispatch();
   const addToCarts = async (item) => {
-    dispatch(addToCart({
-      id: item.id,
-      foodname: item.foodname,
-      price: item.price,
-      img: item.img,
-      quantity: 1,
-      tprice: item.price,
-    }));
+    dispatch(
+      addToCart({
+        id: item.id,
+        foodname: item.foodname,
+        price: item.price,
+        img: item.img,
+        quantity: 1,
+        tprice: item.price,
+      })
+    );
   };
-  const decreaseQuantity=(item)=>{
-    console.log(mySelector)
-    dispatch(decqty({
-      id:item.id
-    }))
-  }
-  let grandTotalPrice=0;
-  let GrandTotal=mySelector.map((key)=>{
-     grandTotalPrice+=key.tprice
-  })
+  const decreaseQuantity = (item) => {
+    console.log(mySelector);
+    dispatch(
+      decqty({
+        id: item.id,
+      })
+    );
+  };
+  let grandTotalPrice = 0;
+  let GrandTotal = mySelector.map((key) => {
+    grandTotalPrice += key.tprice;
+  });
+
+  const handleCancel = () => {
+    displayCart(false);
+  };
+
   return (
     <>
-    <div className="cart-container " id={state}>
-    <div className="cart-page">
-        <div>
-        <h1 className="order-summary">Order Summary</h1>
-        {
-            mySelector.map((key)=>{
-                return(
-                    <div className="cart-page-1" >
-            <div className="cart-page-1-1">{key.foodname} </div>
-            <div className="cart-page-1-1">{key.quantity}</div>
-            <div>
-                <button className="in-cart-button" style={{backgroundColor:"#3F51B5"}} onClick={()=>{addToCarts(key)}} >+</button>
-                <button className="in-cart-button" style={{backgroundColor:"#F50057"}} onClick={()=>{decreaseQuantity(key)}}>-</button>
+      <div className="cart-container ">
+        <div className="cart-page">
+          <div>
+            <h1 className="order-summary">Order Summary</h1>
+            {mySelector.map((key) => {
+              return (
+                <div className="cart-page-1">
+                  <div className="cart-page-1-1">{key.foodname} </div>
+                  <div className="cart-page-1-1">{key.quantity}</div>
+                  <div>
+                    <button
+                      className="in-cart-button"
+                      style={{ backgroundColor: "#3F51B5" }}
+                      onClick={() => {
+                        addToCarts(key);
+                      }}
+                    >
+                      +
+                    </button>
+                    <button
+                      className="in-cart-button"
+                      style={{ backgroundColor: "#F50057" }}
+                      onClick={() => {
+                        decreaseQuantity(key);
+                      }}
+                    >
+                      -
+                    </button>
+                  </div>
+                </div>
+              );
+            })}
+            <div className="cart-page-1-1" style={{ marginLeft: "20px" }}>
+              Total (INR) : {grandTotalPrice}{" "}
             </div>
+            <div className="cart-part-1-2">
+              <Link to="/Checkout">
+                <button
+                  className="in-cart-button"
+                  style={{ backgroundColor: "#3F51B5" }}
+                >
+                  SAVE AND CHECKOUT
+                </button>
+              </Link>
+              <button
+                className="in-cart-button"
+                style={{ backgroundColor: "#fff", color: "#3F51B5" }}
+                onClick={handleCancel}
+              >
+                CANCEL
+              </button>
             </div>
-                )
-            })
-        }
-        <div className="cart-page-1-1" style={{marginLeft:"20px"}}>Total (INR) : { grandTotalPrice} </div>
-        <div className='cart-part-1-2'>
-            <Link to="/Checkout"><button className="in-cart-button" style={{backgroundColor:"#3F51B5"}} >SAVE AND CHECKOUT</button></Link>
-            <button  className="in-cart-button" style={{backgroundColor:"#fff", color:"#3F51B5"}} onClick={()=>{
-              // dispatch(setHide("hide"))
-             setState("hide")
-            }}>CANCEL</button>
+          </div>
         </div>
-        </div>
-     </div>
-     </div>
+      </div>
     </>
-  )
-}
-export default CartPage
+  );
+};
+export default CartPage;
